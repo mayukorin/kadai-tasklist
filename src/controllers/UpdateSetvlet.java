9ad5be15c2f4bcc4e1273f4dab21048688c7e777
mayukorin <19t7b2fx@gmail.com>
2020-04-07 11:47:20 +0900

    Add flush message

--- src/controllers/UpdateSetvlet.java
+++ src/controllers/UpdateSetvlet.java
@@ -50,6 +50,7 @@ public class UpdateSetvlet extends HttpServlet {
 
             em.getTransaction().begin();
             em.getTransaction().commit();
+            request.getSession().setAttribute("flush", "更新が完了しました。");
             em.close();
 
             request.getSession().removeAttribute("task_id");
